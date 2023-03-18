810ce0b13935fcccd8c31cee6144ae620688add7
Carsten Hutsch <c.hutsch@googlemail.com>
2023-03-18 17:21:53 +0100

    ADD: Laden Basic Rom Liste

--- xStationBackupManager/Manager/RomManager.cs
+++ xStationBackupManager/Manager/RomManager.cs
@@ -1,9 +1,30 @@
-﻿using xStationBackupManager.Contracts;
+﻿using Autofac;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Documents;
+using xStationBackupManager.Contracts;
 
 namespace xStationBackupManager.Manager {
     internal class RomManager : IRomManager {
+        private readonly ILifetimeScope _scope;
+
+        public RomManager(ILifetimeScope scope) {
+            _scope = scope;
+        }
+
         public IRom[] GetRoms(string path) {
-            return null;
+            if (!Directory.Exists(path)) return new IRom[0];
+
+            var result = new List<IRom>();
+            foreach (var file in Directory.GetFiles(path)) {
+                if (!file.Contains(".7z")) continue;
+                var rom = _scope.Resolve<IRom>();
+                var fileInfo = new FileInfo(file);
+                rom.Name = fileInfo.Name.Replace(fileInfo.Extension, string.Empty);
+                rom.Path = file;
+                result.Add(rom);
+            }
+            return result.ToArray();
         }
     }
 }
--- xStationBackupManager/ViewModels/MainWindowViewModel.cs
+++ xStationBackupManager/ViewModels/MainWindowViewModel.cs
@@ -8,21 +8,29 @@ using xStationBackupManager.Enums;
 namespace xStationBackupManager.ViewModels {
     internal class MainWindowViewModel : ViewModelBase, IMainWindowViewModel {
         private readonly IOptionsManager _options;
+        private readonly IRomManager _romManager;
 
         public RelayCommand CloseCommand { get; }
 
         public RelayCommand SettingsCommand { get; }
 
-        public string RomsPath {
-            get;
-            set;
-        }
+        public string RomsPath { get; set; }
 
-        public MainWindowViewModel(IOptionsManager options) {
+        public RomViewModel[] Roms { get; set; }
+
+        public MainWindowViewModel(IOptionsManager options, IRomManager romManager) {
             _options = options;
             RomsPath = _options.GetOption(Options.RomPath).GetValue();
             CloseCommand = new RelayCommand(CloseCommandExecuted);
             SettingsCommand = new RelayCommand(SettingsCommandExecuted);
+            _romManager = romManager;
+
+            var roms = _romManager.GetRoms(RomsPath);
+            var romVms = new RomViewModel[roms.Length];
+            for(int i = 0; i < roms.Length; i++) {
+                romVms[i] = new RomViewModel(roms[i]);
+            }
+            Roms = romVms;
         }
 
         private void CloseCommandExecuted() {
