3441d49d028bed1c2ee8c3b7a153f88c2895ca99
Carsten Hutsch <c.hutsch@googlemail.com>
2023-03-18 14:56:43 +0100

    ADD: Menu und command base

--- xStationBackupManager/ViewModels/MainWindowViewModel.cs
+++ xStationBackupManager/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,17 @@
-﻿using xStationBackupManager.Contracts;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using xStationBackupManager.Contracts;
 using xStationBackupManager.Enums;
 
 namespace xStationBackupManager.ViewModels {
     internal class MainWindowViewModel : ViewModelBase, IMainWindowViewModel {
         private readonly IOptionsManager _options;
+
+        public RelayCommand CloseCommand { get; }
+
+        public RelayCommand SettingsCommand { get; }
 
         public string RomsPath {
             get;
@@ -13,6 +21,16 @@ namespace xStationBackupManager.ViewModels {
         public MainWindowViewModel(IOptionsManager options) {
             _options = options;
             RomsPath = _options.GetOption(Options.RomPath).GetValue();
+            CloseCommand = new RelayCommand(CloseCommandExecuted);
+            SettingsCommand = new RelayCommand(SettingsCommandExecuted);
+        }
+
+        private void CloseCommandExecuted() {
+            Application.Current.Shutdown();
+        }
+
+        private void SettingsCommandExecuted() {
+
         }
     }
 }
